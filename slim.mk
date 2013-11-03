5c7cd6503ea4809f13cb39e1769257667ae49b19
Josue Rivera <prbassplayer@gmail.com>
2013-11-04 00:15:16 +0100

    Update dependencies and cleanup mk files
    Change-Id: I4d754918380da46ad9d660325f5f2cccb46bf902
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- slim.mk
+++ slim.mk
@@ -1,21 +1,21 @@
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/slim/config/gsm.mk)
-
-# Release name
-PRODUCT_RELEASE_NAME := t0lte
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/samsung/t0lte/full_t0lte.mk)
 
+# Inherit some common slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/slim/config/gsm.mk)
+
 # Inherit torch settings
 $(call inherit-product, vendor/slim/config/common_ledflash.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := t0lte
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := t0lte
